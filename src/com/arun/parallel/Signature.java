439ca236a918d74d461a62f14be86f1a046738a2
strider90arun <strider90arun@gmail.com>
2017-06-11 00:19:20 -0700

    Signature Builder pattern

--- src/com/arun/parallel/Signature.java
+++ src/com/arun/parallel/Signature.java
@@ -9,17 +9,7 @@ public class Signature {
 	private List<? extends Object> args;
 	private List<Class<? extends Object>> argTypes;
 
-	public Signature(String name, Class<? extends Object> returnType) {
-		super();
-		this.name = name;
-		this.returnType = returnType;
-	}
-	
-	public Signature(String name, Class<? extends Object> returnType, List<? extends Object> args, List<Class<? extends Object>> argTypes) {
-		this(name, returnType);
-		this.args = args;
-		this.argTypes = argTypes;
-	}
+	private Signature() {}
 
 	public String getName() {
 		return name;
@@ -35,5 +25,49 @@ public class Signature {
 
 	public List<? extends Object> getArgs() {
 		return args;
+	}
+
+	public static ReturnType method(String methodName) {
+		return new Signature.SignatureBuilder(methodName);
+	}
+
+	public interface ReturnType {
+		Build returnType(Class<? extends Object> returnType);
+	}
+
+	public interface Build {
+		Build argsList(List<? extends Object> args);
+
+		Build argTypes(List<Class<? extends Object>> argTypes);
+
+		Signature build();
+	}
+
+	public static class SignatureBuilder implements ReturnType, Build {
+		private Signature signature = new Signature();
+
+		public SignatureBuilder(String method) {
+			signature.name = method;
+		}
+
+		public SignatureBuilder returnType(Class<? extends Object> returnType) {
+			signature.returnType = returnType;
+			return this;
+		}
+
+		public SignatureBuilder argsList(List<? extends Object> args) {
+			signature.args = args;
+			return this;
+		}
+
+		public SignatureBuilder argTypes(List<Class<? extends Object>> argTypes) {
+			signature.argTypes = argTypes;
+			return this;
+		}
+
+		public Signature build() {
+			return signature;
+		}
+
 	}
 }
