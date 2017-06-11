707fe0a47587a4c457b60d3bca47f1f40aa7ed01
strider90arun <strider90arun@gmail.com>
2017-06-11 00:17:19 -0700

    Deleting Boolean and Object consumer

--- src/com/arun/parallel/BooleanConsumer.java
+++
@@ -1,7 +0,0 @@
-package com.arun.parallel;
-
-@FunctionalInterface
-public interface BooleanConsumer {
-
-	Boolean accept();
-}
--- src/com/arun/parallel/ObjectConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.arun.parallel;
-
-import java.io.Serializable;
-
-@FunctionalInterface
-public interface ObjectConsumer {
-
-	Boolean accept(Serializable obj);
-}
