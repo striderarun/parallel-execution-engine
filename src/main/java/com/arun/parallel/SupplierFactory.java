c8e335216fd5711c98126de73f42bbf59b2f4a25
strider90arun <strider90arun@gmail.com>
2017-06-24 19:15:03 -0400

    Documenting the Supplier Factory methods

--- src/main/java/com/arun/parallel/SupplierFactory.java
+++ src/main/java/com/arun/parallel/SupplierFactory.java
@@ -7,6 +7,18 @@ import java.util.function.Supplier;
 
 public class SupplierFactory {
 
+	/**
+	 * Generic method which converts any method signature to instance of a Supplier
+	 *
+	 * @param obj
+	 * @param methodName
+	 * @param type
+	 * @param signature
+	 * @param <T>
+	 * @return
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 */
 	public static <T> Supplier<T> createGenericReflectionSupplier(Object obj, String methodName, Class<? extends Object> type, Signature signature) throws NoSuchMethodException, SecurityException {
 		if (signature.getArgs() != null && signature.getArgs().size() > 0) {
 			return createGenericReflectionSupplierWithArgs(obj, methodName, type, signature.getArgs(), signature.getArgTypes().toArray(new Class[signature.getArgTypes().size()]));
@@ -14,7 +26,21 @@ public class SupplierFactory {
 			return createGenericReflectionSupplierNoArgs(obj, methodName, type);
 		}
 	}
-	
+
+	/**
+	 * Generic method which converts a method signature with arguments to a Supplier instance
+	 *
+	 * @param obj
+	 * @param methodName
+	 * @param type
+	 * @param args
+	 * @param argTypes
+	 * @param <T>
+	 * @param <E>
+	 * @return
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 */
 	public static <T,E> Supplier<T> createGenericReflectionSupplierWithArgs(Object obj, String methodName, Class<? extends Object> type, List<? extends Object> args, Class<E>... argTypes) throws NoSuchMethodException, SecurityException {
 		Method argsMethod = obj.getClass().getMethod(methodName, argTypes);		
 		return () -> {
@@ -27,7 +53,18 @@ public class SupplierFactory {
 			return t;
 		};		
 	}
-	
+
+	/**
+	 * Generic method which converts a method signature without arguments to a Supplier instance
+	 *
+	 * @param obj
+	 * @param methodName
+	 * @param type
+	 * @param <T>
+	 * @return
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 */
 	public static <T> Supplier<T> createGenericReflectionSupplierNoArgs(Object obj, String methodName, Class<? extends Object> type) throws NoSuchMethodException, SecurityException {
 		Method noArgsMethod = obj.getClass().getMethod(methodName);
 		return () -> {
