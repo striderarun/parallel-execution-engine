75efd2192fae3a93db85a689b8ec31c0ee207559
strider90arun <strider90arun@gmail.com>
2017-06-29 13:44:31 -0700

    Build Signature from MethodModel

--- execution-engine/src/main/java/com/arun/parallel/Signature.java
+++ execution-engine/src/main/java/com/arun/parallel/Signature.java
@@ -1,5 +1,7 @@
 package com.arun.parallel;
 
+import com.arun.MethodModel;
+
 import java.util.List;
 
 public class Signature {
@@ -27,47 +29,12 @@ public class Signature {
 		return args;
 	}
 
-	public static ReturnType method(String methodName) {
-		return new Signature.SignatureBuilder(methodName);
-	}
-
-	public interface ReturnType {
-		Build returnType(Class<? extends Object> returnType);
-	}
-
-	public interface Build {
-		Build argsList(List<? extends Object> args);
-
-		Build argTypes(List<Class<? extends Object>> argTypes);
-
-		Signature build();
-	}
-
-	public static class SignatureBuilder implements ReturnType, Build {
-		private Signature signature = new Signature();
-
-		public SignatureBuilder(String method) {
-			signature.name = method;
-		}
-
-		public SignatureBuilder returnType(Class<? extends Object> returnType) {
-			signature.returnType = returnType;
-			return this;
-		}
-
-		public SignatureBuilder argsList(List<? extends Object> args) {
-			signature.args = args;
-			return this;
-		}
-
-		public SignatureBuilder argTypes(List<Class<? extends Object>> argTypes) {
-			signature.argTypes = argTypes;
-			return this;
-		}
-
-		public Signature build() {
-			return signature;
-		}
-
+	public static Signature method(MethodModel methodModel) {
+		Signature signature = new Signature();
+		signature.name = methodModel.getName();
+		signature.returnType = methodModel.getReturnType();
+		signature.argTypes = methodModel.getArgTypes();
+		signature.args = methodModel.getArgs();
+		return signature;
 	}
 }
